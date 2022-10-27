7becf5f8868ab40d85ed22a066d9394602c4c7c7
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-10-27 16:41:47 +1100

    fix ssl

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 
-rocess.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-       ssl:true,
+       ssl:{
+           rejectUnauthorized: false
+       }
     }
 });
 

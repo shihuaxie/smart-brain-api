8e4e518b73fb0440d0194ae776448b3758520c94
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-10-27 16:34:07 +1100

    fix

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 
+rocess.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
     client: 'pg',
     connection: {

dee8572bfeab9a71d681ff4c5b02eee839fe8b23
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-10-27 14:52:16 +1100

    changing response

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 
-app.get('/', (req, res) => {res.send('success')})
+app.get('/', (req, res) => {res.send('it is working!')})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})

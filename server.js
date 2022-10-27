2356321e83bfe3b624c1a54f15b45818cefb9502
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-10-27 16:25:36 +1100

    adding database connection

--- server.js
+++ server.js
@@ -16,11 +16,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-spherical-96702',
-        port: 5432,
-        user: 'shihuaxie',
-        password: '',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+       ssl:true,
     }
 });
 

bfcc6cb8bcfd87d7a348c415ab6dc4ec010c00c4
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-06-09 17:18:52 +1000

    feat: create models

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config()
+const app = express()
 
 
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000',
 }))
 
+mongoose.connect(process.env.MONGO_URL)
+
+
+//test
 app.get('/test', (req, res) =>{
     res.json('test ok');
 })
 
+//register
+app.post('/register', (req, res) =>{
+    const {name, email, password} = req.body;
+    res.json({name, email, password});
+})
+
+//login
 app.listen(4321);
+

02e8ca21768d81aaf88afb84214983bb94f252ee
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-06-09 20:41:08 +1000

    feat: register and signin page completed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config()
-const app = express()
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
+require('dotenv').config();
+const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'jagdkgjqlw3eq';
 
 app.use(express.json());
 app.use(cors({
@@ -11,20 +16,49 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }))
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect('mongodb+srv://SmartBrain:qMEZDv41qxtwcDbj@cluster0.fkcfmsf.mongodb.net/?retryWrites=true&w=majority')
 
 
 //test
-app.get('/test', (req, res) =>{
+app.get('/test', (req, res) => {
     res.json('test ok');
 })
 
 //register
-app.post('/register', (req, res) =>{
+app.post('/register', async (req, res) => {
     const {name, email, password} = req.body;
-    res.json({name, email, password});
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        //res.json(userDoc);
+        res.json({user: userDoc, message: 'Registration successful'});
+    } catch (err) {
+        //res.status(422).json(err);
+        res.status(422).json({error: err.message});
+    }
 })
 
-//login
+//sign in
+app.post('/signin', async (req, res) => {
+    const {signInEmail, signInPassword} = req.body;
+    const userDoc = await User.findOne({email: signInEmail});
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(signInPassword, userDoc.password);
+        if (passOk) {
+            jwt.sign({signInPassword: userDoc.email, id: userDoc._id}, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json({user: userDoc, message: 'password found'});
+            });
+        } else {
+            res.status(422).json('password not found');
+        }
+    } else {
+        res.json('not found');
+    }
+})
+
 app.listen(4321);
 

6d6efcd6204897ded7ef313e0133884b704406bb
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-06-11 19:56:18 +1000

    chore

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://sylvia-smart-brain.netlify.app',
+   // 'https://sylvia-smart-brain.netlify.app',
 ];
 
 app.use(cors({
